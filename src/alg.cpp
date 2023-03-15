984c4f2b71e1567791b4a07cbd360411658d5795
AndreyAkimov <124913657+AkimovAndrey3@users.noreply.github.com>
2023-03-15 21:50:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,55 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int k = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i+1; j < len; j++) {
+      if ((arr[i] + arr[j]) == value) {
+        k++;
+      }
+    }
+  }
+  return k;
 }
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int k = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int j = len - 1; i < j; j--) {
+      if ((arr[i] + arr[j]) == value) {
+        k++;
+      }
+    }
+  }
+  return k;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int middle = 0;
+  int k = 0;
+  for (int i = 0; i < len-1; i++) {
+    int left = i;
+    int right = len - 1;
+    while (right > left) {
+      middle = (right + left) / 2;
+      if ((arr[i]+arr[middle]) == value) {
+        k++;
+        int mid2 = middle + 1;
+        while (((arr[i] + arr[mid2] == value)) && (right > middle)) {
+          k++;
+          middle++;
+        }
+        while (((arr[i] + arr[mid2]) == value) && (left < mid2)) {
+          k++;
+          mid2--;
+        }
+        break;
+      }
+      if ((arr[i] + arr[middle]) > value) {
+        right = middle;
+      } else {
+        left = middle;
+      }
+    }
+  }
+  return k;
 }
