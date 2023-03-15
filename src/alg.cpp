d6463ee3d0e4ab97e7536a0ea33c9cc909ad8772
AndreyAkimov <124913657+AkimovAndrey3@users.noreply.github.com>
2023-03-15 23:48:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@ int countPairs1(int *arr, int len, int value) {
   int k = 0;
   for (int i = 0; i < len-1; i++) {
     for (int j = i+1; j < len; j++) {
-      if ((arr[i] + arr[j]) == value) {
+      if (arr[i] + arr[j] == value) {
         k++;
       }
     }
@@ -14,8 +14,8 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   int k = 0;
   for (int i = 0; i < len-1; i++) {
-    for (int j = len - 1; i < j; j--) {
-      if ((arr[i] + arr[j]) == value) {
+    for (int j = len-1; i < j; j--) {
+      if (arr[i] + arr[j] == value) {
         k++;
       }
     }
@@ -24,30 +24,29 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int middle = 0;
   int k = 0;
-  for (int i = 0; i < len-1; i++) {
-    int leftBorder = i;
-    int rightBorder = len;
-    while (rightBorder-1 > leftBorder) {
-      middle = (rightBorder + leftBorder) / 2;
-      if ((arr[i] + arr[middle]) == value) {
+  for (int i = 0; i < len - 1; i++) {
+    int leftBorder = i, rightBorder = len;
+    while (rightBorder - 1 > leftBorder) {
+      int mid = (leftBorder + rightBorder) / 2;
+      if (arr[i] + arr[mid] == value) {
         k++;
-        int mid2 = middle+1;
-        while (((arr[i] + arr[mid2] == value)) && (rightBorder > mid2)) {
+        int mid2 = mid + 1;
+        while (arr[i] + arr[mid2] == value && mid2 < rightBorder) {
           k++;
           mid2++;
         }
-        while (((arr[i] + arr[mid2]) == value) && (leftBorder < mid2)) {
+        mid2 = mid - 1;
+        while (arr[i] + arr[mid2] == value && mid2 > leftBorder) {
           k++;
           mid2--;
         }
         break;
       }
-      if ((arr[i]+arr[middle]) > value) {
-        rightBorder = middle;
+      if (arr[i] + arr[mid] > value) {
+        rightBorder = mid;
       } else {
-        leftBorder = middle;
+        leftBorder = mid;
       }
     }
   }
